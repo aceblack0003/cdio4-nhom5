cf4549332a3c5f2ea4ac1dc9576953b071c61b1d
HIEU TRUONG <aceblack0003@gmail.com>
2024-06-13 21:18:51 +0700

    exception

--- src/main/java/vn/example/itviec/controller/HelloController.java
+++ src/main/java/vn/example/itviec/controller/HelloController.java
@@ -3,10 +3,16 @@ package vn.example.itviec.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.example.itviec.service.error.IdInvalidException;
+
 @RestController
 public class HelloController {
-     @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello World";
+    
+
+    @GetMapping("/")
+    public String getHelloWorld() throws IdInvalidException {
+        if (true)
+            throw new IdInvalidException("check");
+        return ("hello");
     }
 }
--- src/main/java/vn/example/itviec/controller/UserController.java
+++ src/main/java/vn/example/itviec/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.example.itviec.domain.User;
 import vn.example.itviec.service.UserService;
+import vn.example.itviec.service.error.IdInvalidException;
 
 @RestController
 public class UserController {
@@ -24,31 +25,34 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createNewuser(@RequestBody User postManuser) {
-       
-        User user=this.userService.handleCreateUser(postManuser);
+    public ResponseEntity<User> createNewUser(@RequestBody User postManuser) {
+
+        User user = this.userService.handleCreateUser(postManuser);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteuser(@PathVariable("id") long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+        if(id>=1500){
+            throw new IdInvalidException("Id khong lon hon 1500");
+        }
         this.userService.handleDeleteUser(id);
-        return ResponseEntity.status(HttpStatus.OK).body("user") ;
+        return ResponseEntity.status(HttpStatus.OK).body("user");
     }
-    // fetch users by id
+
     @GetMapping("/users/{id}")
-    public ResponseEntity<User> getuserById(@PathVariable("id") long id) {
-        User fetchUser=this.userService.fetchUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        User fetchUser = this.userService.fetchUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body(fetchUser);
     }
 
-    // fetch all userss
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllusers() {
+    public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
     }
+
     @PutMapping("/users")
-    public ResponseEntity<User> updateusers(@RequestBody User user) {
+    public ResponseEntity<User> updateUsers(@RequestBody User user) {
         User updateUser = this.userService.handleUpdateUser(user);
         return ResponseEntity.status(HttpStatus.OK).body(updateUser);
     }
