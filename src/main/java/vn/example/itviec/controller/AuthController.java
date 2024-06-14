44d47c8bbe0a60fc76b4926e298d5d9399f8bfa8
HIEU TRUONG <aceblack0003@gmail.com>
2024-06-14 18:53:56 +0700

    jwt complete

--- src/main/java/vn/example/itviec/controller/AuthController.java
+++ src/main/java/vn/example/itviec/controller/AuthController.java
@@ -4,17 +4,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import vn.example.itviec.domain.dto.RestLoginDTO;
 
 import jakarta.validation.Valid;
 import vn.example.itviec.domain.dto.LoginDTO;
+import vn.example.itviec.domain.dto.RestLoginDTO;
 import vn.example.itviec.util.SecurityUtil;
 
 @RestController
 public class AuthController {
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
 
@@ -22,7 +24,6 @@ public class AuthController {
             SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
-
     }
 
     @PostMapping("/login")
@@ -34,8 +35,10 @@ public class AuthController {
         // xác thực người dùng => cần viết hàm loadUserByUsername
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
-        // create token
+        // create a token
         String access_token = this.securityUtil.createToken(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         RestLoginDTO res = new RestLoginDTO();
         res.setAccessToken(access_token);
         return ResponseEntity.ok().body(res);
