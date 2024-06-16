35b8841924430e8e2bc546e9db2df733f4785c87
HIEU TRUONG <aceblack0003@gmail.com>
2024-06-16 18:58:32 +0700

    get/update/delete company

--- src/main/java/vn/example/itviec/domain/Company.java
+++ src/main/java/vn/example/itviec/domain/Company.java
@@ -2,15 +2,20 @@ package vn.example.itviec.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.example.itviec.util.SecurityUtil;
 
 @Table(name = "companies")
 @Entity
@@ -31,6 +36,7 @@ public class Company {
 
     private String logo;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
 
     private Instant updatedAt;
@@ -38,5 +44,22 @@ public class Company {
     private String createdBy;
 
     private String updatedBy;
-}
 
+    @PrePersist
+    public void handleCreateAt() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        this.updatedAt = Instant.now();
+    }
+}
