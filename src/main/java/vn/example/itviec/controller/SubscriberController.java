f54c9e0e4becd6421d3b10445929eb2d705d1e28
HIEU TRUONG <aceblack0003@gmail.com>
2024-06-26 18:12:02 +0700

    crud subscriber

---
+++ src/main/java/vn/example/itviec/controller/SubscriberController.java
@@ -0,0 +1,49 @@
+package vn.example.itviec.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import vn.example.itviec.domain.Subscriber;
+import vn.example.itviec.service.SubscriberService;
+import vn.example.itviec.util.annotation.ApiMessage;
+import vn.example.itviec.util.error.IdInvalidException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SubscriberController {
+    private final SubscriberService subscriberService;
+
+    public SubscriberController(SubscriberService subscriberService) {
+        this.subscriberService = subscriberService;
+    }
+
+    @PostMapping("/subscribers")
+    @ApiMessage("Create a subscriber")
+    public ResponseEntity<Subscriber> create(@Valid @RequestBody Subscriber sub) throws IdInvalidException {
+        // check email
+        boolean isExist = this.subscriberService.isExistsByEmail(sub.getEmail());
+        if (isExist == true) {
+            throw new IdInvalidException("Email " + sub.getEmail() + " đã tồn tại");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.subscriberService.create(sub));
+    }
+
+    @PutMapping("/subscribers")
+    @ApiMessage("Update a subscriber")
+    public ResponseEntity<Subscriber> update(@RequestBody Subscriber subsRequest) throws IdInvalidException {
+        // check id
+        Subscriber subsDB = this.subscriberService.findById(subsRequest.getId());
+        if (subsDB == null) {
+            throw new IdInvalidException("Id " + subsRequest.getId() + " không tồn tại");
+        }
+        return ResponseEntity.ok().body(this.subscriberService.update(subsDB, subsRequest));
+    }
+
+}
---
+++ src/main/java/vn/example/itviec/repository/SubscriberRepository.java
@@ -0,0 +1,14 @@
+package vn.example.itviec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import vn.example.itviec.domain.Subscriber;
+
+@Repository
+public interface SubscriberRepository extends JpaRepository<Subscriber, Long>,
+                JpaSpecificationExecutor<Subscriber> {
+
+        boolean existsByEmail(String email);
+}
